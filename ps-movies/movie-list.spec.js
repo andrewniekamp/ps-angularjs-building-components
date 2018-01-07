d6c5418fdfd676ce8bf2dd1b8ec138f5a8730b76
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2018-01-07 14:35:20 -0500

    Add logic to pass test that component can be created

--- ps-movies/movie-list.spec.js
+++ ps-movies/movie-list.spec.js
@@ -2,12 +2,21 @@ describe("The movieList component", function() {
 
     beforeEach(module("psMovies"));
 
-    var movieList;
+    var moviesList;
     beforeEach(inject(function($componentController) {
+        // Simply need to construct from given $componentController mock made available by AngularJS
+        moviesList = $componentController("movieList", {
+            // Remember our movie list component requires $http
+            // If we don't specify an override, AngularJs will still use it by default. In this case, that's ok
 
+            // Howover, must mock the scope, we can use an empty object, as we are just testing for component creation
+            $scope: {}
+        });
     }));
 
     it("can be created", function() {
         expect(moviesList).toBeDefined();
+        // Expect $onInit lifecycle hook to work
+        expect(moviesList.$onInit).toBeDefined();
     });
 })
