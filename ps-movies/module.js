44fe04ea1836cc3340262690825e88ee0176934b
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2018-01-07 14:47:40 -0500

    Add simple nav and routing for about and list

--- ps-movies/module.js
+++ ps-movies/module.js
@@ -1,6 +1,28 @@
 (function() {
     "use strict";
 
-    angular.module("psMovies", []);
+    // Save module in variable to configure below
+    var module = angular.module("psMovies", [
+        // List dependencies here
+        // Routing module
+        "ngRoute"
+    ]);
+    
+    // Configure routes
+    module.config(function($routeProvider) {
+        $routeProvider
+        .when("/list", {
+            template: "<movie-list></movie-list>"
+        })
+        .when("/about", {
+            template: "<app-about></app-about>"
+        })
+        .otherwise({redirectTo: "/list"});
+    });
+
+    // We have movie list-component, here is a quick inline definition for app-about
+    module.component("appAbout", {
+        template: "This is the about page"
+    });
 
 }());
