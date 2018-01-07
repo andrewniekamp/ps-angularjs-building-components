c93f55ec9ab81516dda6dda44914f4010526e22d
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2018-01-07 14:31:06 -0500

    Add test for movie-list component

---
+++ ps-movies/movie-list.spec.js
@@ -0,0 +1,13 @@
+describe("The movieList component", function() {
+
+    beforeEach(module("psMovies"));
+
+    var movieList;
+    beforeEach(inject(function($componentController) {
+
+    }));
+
+    it("can be created", function() {
+        expect(moviesList).toBeDefined();
+    });
+})
