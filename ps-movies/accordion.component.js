9b8e117af4b9b1880344f03de2707a74ca9f5495
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2018-01-08 14:37:26 -0500

    Add attribute binding for accordion-panel component

--- ps-movies/accordion.component.js
+++ ps-movies/accordion.component.js
@@ -10,8 +10,15 @@
     });
 
     module.component("accordionPanel", {
+        bindings: {
+            "heading": "@" // attribute binding, just text
+        },
+        controllerAs: "vm",
         transclude: true,
-        template:   "<div class='panel panel-default'" +
+        template:   "<div class='panel panel-default'>" +
+                        "<div class='panel-heading'>" +
+                            "<h4 class='panel-title'>{{vm.heading}}</h4>" +
+                        "</div>" +
                         "<div class='panel-body' ng-transclude>" +
                         "</div>" +
                     "</div>"
