86d11bd69011a328c0a13db418dba2667039e30b
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2018-01-08 13:45:39 -0500

    Update movie rating component to be more decoupled

--- ps-movies/movie-rating.component.js
+++ ps-movies/movie-rating.component.js
@@ -3,23 +3,32 @@
 
     var module = angular.module("psMovies");
 
+    function buildEntries(value, max) {
+        var entries = [];
+
+        for (var i = 1; i <= max; i++) {
+            var icon = i <= value ? "glyphicon-star" : "glyphicon-star-empty";
+            entries.push(icon);
+        }
+        return entries;
+    }
+
     module.component("movieRating", {
         templateUrl: "/ps-movies/movie-rating.component.html",
         bindings: { // Describe properties on controller instance
-            value: "<" // value name is arbitrary
+            value: "<", // value name is arbitrary
+            max: "<"
         },
-        // Override component
-        transclude: true,
         controllerAs: "vm",
         controller: function() {
             var vm = this;
 
             vm.$onInit = function() {
-                vm.entries = new Array(vm.value);
+                vm.entries = buildEntries(vm.value, vm.max);
             };
 
             vm.$onChanges = function() {
-                vm.entries = new Array(vm.value);
+                vm.entries = buildEntries(vm.value, vm.max);
             };
         }
     });
